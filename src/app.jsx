4eb78ca8414567d1a2ae25106f8e0e775978ea22
Meche221 <binaymeche22@gmail.com>
2025-12-16 11:41:55 +0545

    fixed the casing

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import {ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from "./components/Navbar";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
